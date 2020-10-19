fa95a9de9795fad3f11e8d612489aa29bee92fde
FrancisNYPC <42808901+nyproton@users.noreply.github.com>
2020-10-19 10:51:05 -0400

    Add files via upload

---
+++ SpotStats/Script.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+using System.Runtime.InteropServices;
+using VMS.TPS.Common.Model.API;
+using SpotStats;
+ 
+namespace VMS.TPS
+{
+    public class Script
+    {
+        [DllImport("kernel32")]
+        public static extern bool AllocConsole();
+        public void Execute(ScriptContext context, Window window)
+        {
+            // AllocConsole();
+            // Console.WriteLine("Start...");
+            MainView mainView = new MainView(context);
+            window.Title = "Scanning Spot Statistics";
+            window.Content = mainView;
+            window.SizeToContent = SizeToContent.WidthAndHeight;
+            window.ResizeMode = ResizeMode.NoResize;
+        }
+    }
+}
